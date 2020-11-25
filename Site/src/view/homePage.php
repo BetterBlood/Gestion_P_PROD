55ef49fa0fefa068ca6227309d9134b89b0af9d3
RicoDelgad0 <ricprt2000@gmail.com>
2020-11-25 13:46:19 +0100

    modif

--- Site/src/view/homePage.php
+++ Site/src/view/homePage.php
@@ -8,11 +8,16 @@
     Description : Page d'accueil du site
   -->
   <meta charset="utf-8">
-  <title>Titre du site</title>
+  <title>P_Prod</title>
   <link rel="stylesheet" href="../ressources/css/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <script src="https://kit.fontawesome.com/a3d242c9b3.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
+
+<h1>P_Prod</h1>
+
 </body>
 
 <footer>
