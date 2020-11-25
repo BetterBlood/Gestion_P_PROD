e07957c6b489279a77552b87d16cf6073a17f13e
RicoDelgad0 <ricprt2000@gmail.com>
2020-11-25 13:01:02 +0100

    Création de MVC

---
+++ Site/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="fr-ch">
+
+<head>
+  <!--
+    Auteurs : Julien Leresche, Jeremiah Steiner et Ricardo Delgado Miranda
+    Date : 25.11.2020
+    Description : Page index envoyant sur la page principale du site
+  -->
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <meta http-equiv="refresh" content="0; url = /view/homePage.php">
+</head>
+
+<body>
+</body>
+
+<footer>
+</footer>
+
+</html>
---
+++ Site/src/view/homePage.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="fr-ch">
+
+<head>
+  <!--
+    Auteurs : Julien Leresche, Jeremiah Steiner et Ricardo Delgado Miranda
+    Date : 25.11.2020
+    Description : Page d'accueil du site
+  -->
+  <meta charset="utf-8">
+  <title>Titre du site</title>
+  <link rel="stylesheet" href="../ressources/css/style.css">
+</head>
+
+<body>
+</body>
+
+<footer>
+</footer>
+
+</html>
