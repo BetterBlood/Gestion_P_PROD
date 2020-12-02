46abc00a5943dbc5b66b786cebcfad8771c52a4a
dahïr <better_blood@hotmail.com>
2020-12-02 15:26:13 +0100

    homeLinks

--- Site/src/controller/home.php
+++ Site/src/controller/home.php
@@ -3,50 +3,65 @@
 
     $database = new Database();
 
-    $projects = $database->getAllProjects();
+    $projects = $database->getAllProjects(); //TODO : voir ptetre pour faire une limit et du coup plusieurs pages (virtuelle)
     //var_dump($projects);
 ?>
+<head>
 
-<h1>Gestion des projets P_PROD</h1>
-<a href="">Ajouter un projet</a>
-<a href="">Projets archivés</a>
+</head>
 
-<form id="formLogin" action="login.php" méthode="post">
-    <input type="text" id="pseudo" name="pseudo" placeholder="Login">
-    <br>
-    <input type="password" id="password" name="password" placeholder="Password">
-</form>
-<button type="submit" form="formLogin" value="Submit">Se connecter</button>
+<body>
+    <header>
+        <h1>Gestion des projets P_PROD</h1>
 
-<table>
-    <thead>
-        <tr>
-            <th>Date de début</th>
-            <th>Nom</th>
-            <th>Description</th>
-            <th>Enseignants [Initiateur, Coordinateur]</th>
-            <th>Élèves</th>
+        <a href="..\view\addProject.html">Ajouter un projet</a>
+        <a href="..\controller\home.php?archive=true">Projets archivés</a>
 
-        </tr>
-    </thead>
-    <tbody>
-        
-        <?php
-            foreach($projects as $project)
-            {
-                echo '<tr>';
-                echo '<td>' . $project["proStartingDate"] . '</td>';
-                echo '<td>' . $project["proName"] . '</td>';
-                echo '<td>' . $project["proDescription"] . '</td>';
+        <form id="formLogin" action="login.php" méthode="post">
+            <input type="text" id="pseudo" name="pseudo" placeholder="Login">
+            <br>
+            <input type="password" id="password" name="password" placeholder="Password">
+        </form>
+        <button type="submit" form="formLogin" value="Submit">Se connecter</button>
 
-                // TODO : rechercher dans la table des prof les ids ci-dessous et les affichers, 
-                echo '<td>' . $project["idInitiator"] . ', ' . $project["idCoordinator"] . '</td>';
+    </header>
+        <div class="container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date de début</th>
+                        <th>Nom</th>
+                        <th>Description</th>
+                        <th>Enseignants [Initiateur, Coordinateur]</th>
+                        <th>Élèves</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                    <?php
+                        foreach($projects as $project)
+                        {
+                            echo '<tr>';
+                            
+                            echo '<td>' . $project["proStartingDate"] . '</td>';
+                            echo '<td onclick="location.href=' . '\'details.php?idProject=' . $project["idProject"] .'\'"><strong>' . $project["proName"] . '</strong></td>';
+                            echo '<td>' . $project["proDescription"] . '</td>';
 
-                // TODO : vérifier si des élèves sont attribué si oui les afficher sinon afficher :"aucun élèves assignés"
-                echo '<td>prendre le belong ayant l\'idProjet : ' . $project["idProject"] . '</td>';
-                echo '</tr>';
-            }
-        ?>
+                            // TODO : rechercher dans la table des prof les ids ci-dessous et les affichers, 
+                            echo '<td>' . $project["idInitiator"] . ', ' . $project["idCoordinator"] . '</td>';
 
-    </tbody>
-</table>
+                            // TODO : vérifier si des élèves sont attribué si oui les afficher sinon afficher :"aucun élèves assignés"
+                            echo '<td>prendre le belong ayant l\'idProjet : ' . $project["idProject"] . '</td>';
+                            
+                            echo '</tr>';
+                        }
+                    ?>
+
+                </tbody>
+            </table>
+        </div>
+    
+</body>
+<footer>
+
+</footer>
